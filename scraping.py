797bea32a3d6061ffdad83d2ada6e9cefaa5b180
Robertson <Robertson.Melissa1@mayo.edu>
2021-11-28 19:24:49 -0600

    challenge deliverable 2 updatedscraping.py

--- scraping.py
+++ scraping.py
@@ -20,7 +20,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image":featured_image(browser),
         "facts": mars_facts(),
-        "last_modified":dt.datetime.now()
+        "last_modified":dt.datetime.now(),
+        "hemisphere_images":mars_hemispheres(browser)
     }
 
     browser.quit()
@@ -86,6 +87,39 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+    item_list=hemi_soup.find_all('div', class_="item")
+    for item in item_list:
+        text=item.a['href'].split('.')[0].capitalize()
+    try:
+        browser.links.find_by_partial_text(text).click()
+        html = browser.html
+        item_soup = soup(html, 'html.parser')
+        description=item_soup.find_all('div',class_='downloads')
+        title=item_soup.find('h2',class_='title').text
+        image=description[0].a['href']
+        image_url=f'{url}{image}'
+        imgdict={'img_url':image_url,'title':title}
+        hemisphere_image_urls.append(imgdict)
+        browser.back()
+    
+    except Exception as e:
+        return None
+
+    return hemisphere_image_urls
+
+
 
 if __name__ == "__main__":
 
